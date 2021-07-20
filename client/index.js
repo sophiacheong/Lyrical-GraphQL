5063fdcc750b7ed5b9ec08db620a43863d648a7c
sophiacheong <sophiaacheong@gmail.com>
2021-07-19 18:35:47 -0700

    added react router

--- client/index.js
+++ client/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from 'apollo-client';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import { ApolloProvider } from 'react-apollo';
 
 import SongList from './components/SongList';
