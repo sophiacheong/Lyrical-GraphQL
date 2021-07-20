9b794f6405f2bf514b00885aea3a6a7a14870c43
sophiacheong <sophiaacheong@gmail.com>
2021-07-19 19:05:47 -0700

    added on change event for song create, input handle

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 
 class SongCreate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ title: e.target.value })
+  }
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
         <form>
+          <label>Song Title:</label>
+          <input onChange={this.handleChange} value={this.state.title} />
         </form>
       </div>
     );
