5da2ee1cf515f04a073741c7f5a51e6348e147af
sophiacheong <sophiaacheong@gmail.com>
2021-07-28 16:09:16 -0700

    imported fetch song into song detail component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { graphql } from 'react-apollo';
+import fetchSong from '../queries/fetchSong';
 
 class SongDetail extends Component {
   constructor(props) {
