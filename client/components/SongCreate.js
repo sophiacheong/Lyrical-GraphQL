2ae379eb142efa6b84a508d006815e8e7691fa10
sophiacheong <sophiaacheong@gmail.com>
2021-07-27 19:18:22 -0700

    imported query into songlist and song create component

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link, hashHistory } from 'react-router';
+import query from '../queries/fetchSongs';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -20,7 +21,7 @@ class SongCreate extends Component {
     e.preventDefault();
     this.props.mutate({
       variables: { title: this.state.title },
-      refetchQueries: [{  }]
+      refetchQueries: [{ query }]
     })
     .then(() => hashHistory.push('/'))
   }
--- client/components/SongList.js
+++ client/components/SongList.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
 import { Link } from 'react-router';
-import query from '../queries/fetchSongs.js';
+import query from '../queries/fetchSongs';
 
 class SongList extends Component {
   renderSongs() {
