229082f69886969ba29c38ce635eebdbcfa53ba4
sophiacheong <sophiaacheong@gmail.com>
2021-08-02 17:39:37 -0700

    added lyric list component with basic skeleton set up

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router';
 import fetchSong from '../queries/fetchSong';
 
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 class SongDetail extends Component {
   constructor(props) {
@@ -19,6 +20,7 @@ class SongDetail extends Component {
       <div>
         <Link to="/"> Back </Link>
         <h3>{song.title}</h3>
+        <LyricList />
         <LyricCreate id={this.props.params.id} />
       </div>
     )
