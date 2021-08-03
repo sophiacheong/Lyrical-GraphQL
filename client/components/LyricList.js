e75700d1479960f11943a180d6901ef7b0f33733
sophiacheong <sophiaacheong@gmail.com>
2021-08-02 19:13:42 -0700

    added css for liking/like count

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -4,22 +4,23 @@ import { graphql } from 'react-apollo';
 
 class LyricList extends Component {
   onLike(id) {
-    this.props.mutate({
-      variables: { id }
-    })
+    this.props.mutate({ variables: { id } })
   }
 
   renderLyrics() {
-    return this.props.lyrics.map(({ id, content }) => {
+    return this.props.lyrics.map(({ id, content, likes }) => {
       return (
         <li key={id} className="collection-item">
           {content}
-          <i
-            className="material-icons"
-            onClick={() => this.onLike(id)}
-          >
-            thumb_up
-          </i>
+          <div className="vote-box">
+            <i
+              className="material-icons"
+              onClick={() => this.onLike(id)}
+            >
+              thumb_up
+            </i>
+            {likes}
+          </div>
         </li>
       )
     })
